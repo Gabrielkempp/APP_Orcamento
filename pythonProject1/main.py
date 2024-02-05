1865a1a51f278cd25e4eac145d242c0159e76b1e
Gabriel Kempp <gabrielrkempp@gmail.com>
2024-02-05 15:23:20 -0300

    Aula-05-Front

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,6 +1,9 @@
 from tkinter import * #tambem pode usar "import tkinter"
 from tkinter import Tk, ttk
 
+#importando pillow
+from PIL import Image, ImageTk
+
 #Cores
 co0 = "#2e2d2b"  # Preta
 co1 = "#feffff"  # branca
@@ -30,13 +33,25 @@ style.theme_use("clam")
 
 #Criando blocos para dividir a tela
 frameCima = Frame(janela, width=1043, height=50, background=co11, relief= "flat")
-frameCima.grid(row=0, column=0)
+frameCima.grid(row=0, column=0, padx=1)
 
 frameMeio = Frame(janela, width=1043, height=361, background=co11, pady=20, relief= "raised")
-frameMeio.grid(row=1, column=0, pady=1, padx= 0, sticky=NSEW)
+frameMeio.grid(row=1, column=0, pady=2, padx= 1, sticky=NSEW)
 
 frameBaixo = Frame(janela, width=1043, height=300, background=co11, relief= "flat")
-frameBaixo.grid(row=2, column=0, pady=0, padx= 10, sticky=NSEW)
+frameBaixo.grid(row=2, column=0, pady=0, padx= 1, sticky=NSEW)
+
+
+#Trabalhando no frame de Cima
+
+#acessando imagem
+app_img = Image.open('coin.png')
+app_img = app_img.resize((40,40))
+app_img = ImageTk.PhotoImage(app_img)
+
+app_logo = Label(frameCima, image = app_img, text = "  The Wallet", width=250, compound = LEFT, padx = 5 , anchor= NW, font=("Verdana 20 bold"),bg= co11, fg=co1)
+app_logo.place(x=315, y=0)
+
 
 
 janela.mainloop()
