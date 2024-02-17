c8c0793b37aba87b8a732f9d37eedf4285c57629
Gabriel Kempp <gabrielrkempp@gmail.com>
2024-02-17 17:02:17 -0300

    Grafico Pizza

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -49,6 +49,8 @@ frameMeio.grid(row=1, column=0, pady=2, padx= 1, sticky=NSEW)
 frameBaixo = Frame(janela, width=1043, height=300, background=co11, relief= "flat")
 frameBaixo.grid(row=2, column=0, pady=0, padx= 1, sticky=NSEW)
 
+frame_gra_pie = Frame(frameMeio, width=580, height=250, background=co11)
+frame_gra_pie.place(x=415, y=5)
 
 #Trabalhando no frame de Cima
 
@@ -70,16 +72,17 @@ def percentagem():
     style.configure('black.Horizontal.TProgressbar', background= "#2ecc71")
     style.configure("TProgressbar", thickness= 15)
 
-    bar = Progressbar(frameMeio, length=200, style='black.Horizontal.TProgressbar')
+    bar = Progressbar(frameMeio, length=180, style='black.Horizontal.TProgressbar')
+
     bar.place(x=15, y=20)
     bar['value'] = 50
 
     valor=50
 
     l_percentagem = Label(frameMeio, text='{:,.2f}%'.format(valor), anchor=NW, font=('Verdana 12 bold'), bg=co11, fg=co1)
-    l_percentagem.place(x=220, y=17)
+    l_percentagem.place(x=200, y=17)
 
-#Função para grafico bar ---------------------------
+'''#Função para grafico bar ---------------------------
 def percentagem():
     l_nome = Label(frameMeio, text='Porcentagem da Receita Gasta', height=1, anchor=NW, font=('Verdana 12 bold'), bg=co11, fg=co1)
     l_nome.place(x=10, y=-10)
@@ -93,7 +96,7 @@ def percentagem():
     bar.place(x=15, y=20)
 
     l_percentagem = Label(frameMeio, text='{:,.2f}%'.format(bar['value']), anchor=NW, font=('Verdana 12 bold'), bg=co11, fg=co1)
-    l_percentagem.place(x=220, y=17)
+    l_percentagem.place(x=220, y=17)'''
 
 # Função para gráfico de barras ---------------------------
 def grafico_bar():
@@ -142,6 +145,58 @@ def grafico_bar():
     canva.get_tk_widget().place(x=10, y=70)
 
 
+#Função de Resumo total
+def resumo():
+    valor = [500, 600, 420]
+
+    l_linha = Label(frameMeio, text="", width=215, height=1, anchor= NW, font=('Arial 1'), bg=co1)
+    l_linha.place(x=309, y=52)
+    l_sumario = Label(frameMeio, text="Total Da Renda Mensal      ", anchor=NW, font=('Verdana 12 bold'), bg=co11, fg="#2ecc71")
+    l_sumario.place(x=309, y=35)
+    l_sumario = Label(frameMeio, text="R$ {:,.2f}".format(valor[0]), anchor=NW, font=('Arial 17'), bg=co11, fg=co1)
+    l_sumario.place(x=309, y=60)
+
+    l_linha = Label(frameMeio, text="", width=215, height=1, anchor=NW, font=('Arial 1'), bg=co1)
+    l_linha.place(x=309, y=122)
+    l_sumario = Label(frameMeio, text="Total De Gasto Mensal    ", anchor=NW, font=('Verdana 12 bold'), bg=co11,fg="#e74c3c")
+    l_sumario.place(x=309, y=105)
+    l_sumario = Label(frameMeio, text="R$ {:,.2f}".format(valor[1]), anchor=NW, font=('Arial 17'), bg=co11, fg=co1)
+    l_sumario.place(x=309, y=140)
+
+    l_linha = Label(frameMeio, text="", width=215, height=1, anchor=NW, font=('Arial 1'), bg=co1)
+    l_linha.place(x=309, y=207)
+    l_sumario = Label(frameMeio, text="Total Saldo Em Caixa       ", anchor=NW, font=('Verdana 12 bold'), bg=co11,fg="#3498db")
+    l_sumario.place(x=309, y=190)
+    l_sumario = Label(frameMeio, text="R$ {:,.2f}".format(valor[2]), anchor=NW, font=('Arial 17'), bg=co11, fg=co1)
+    l_sumario.place(x=309, y=220)
+
+#Função Grafico Pie
+def grafico_pie():
+    #faça figura e atribua objetos de eixo
+    figura = plt.Figure(figsize=(5, 3), dpi=90, facecolor='#383b41')
+    ax = figura.add_subplot(111)
+
+    lista_valores = [345, 225, 534]
+    lista_categorias = ['Renda', 'Despesa', 'Saldo']
+
+    #only "explode" the 2nd slice(i.e. 'Hogs')
+
+    explode = []
+    for i in lista_categorias:
+        explode.append(0.05)
+
+    ax.pie(lista_valores, explode=explode, wedgeprops=dict(width=0.2), autopct='%1.1f%%', colors=colors,
+               shadow=False, startangle=90, textprops={'color': 'white'})
+    ax.legend(lista_categorias, loc="center right", bbox_to_anchor=(1.50, 0.20))
+
+    canva_categoria = FigureCanvasTkAgg(figura, frame_gra_pie)
+    canva_categoria.get_tk_widget().grid(row=20, column=20, padx=20, pady=20)
+
+
+
+
 percentagem()
 grafico_bar()
+resumo()
+grafico_pie()
 janela.mainloop()
